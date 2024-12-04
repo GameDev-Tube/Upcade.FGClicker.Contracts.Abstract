b8326ad4451fa4e75135306fe8ccdf93845ce98d
Bartek <bartek.blachucki@gmail.com>
2024-12-04 13:46:13 +0100

    build: proxy module deployment, additional docs (wip)

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Solidity smart contract for Fea&Greed: Pepenade Crush minigame, that allows players to store their high scores on a blockchain.
 It uses `@openzeppelin`'s EIP-712 implementation to provide a safe way for users to store backend-validated data.
+
+### Deployment
+1. Create and configure a `.env` file as shown in the `.env.example`
--- ignition/modules/HighScore.ts
+++ ignition/modules/HighScore.ts
@@ -1,12 +1,31 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const HighScoreModule = buildModule("HighScoreModule", (m) => {
-  const _backendSigner = m.getParameter("_backendSigner", "0xfa552727331A154bc19FeF66FCDFcD6eba790718");
+import dotenv from 'dotenv';
+dotenv.config();
 
-  const highScore = m.contract("HighScore", [_backendSigner], {
-  });
+const ProxyModule = buildModule("ProxyModule", (builder) => {
+  // Deploy the implementation contract
+  const implementation = builder.contract("HighScore");
 
-  return { highScore };
+  // Encode the initialize function call
+  const initialize = builder.encodeFunctionCall(implementation, 'initialize', [
+    builder.getParameter("_backendSigner", process.env.BACKEND_WALLET_ADDRESS!)
+  ]);
+
+  // Deploy the ERC1967Proxy pointing to the implementation contract
+  const proxy = builder.contract('ERC1967Proxy', [implementation, initialize]);
+
+  return { proxy };
+});
+
+const HighScoreModule = buildModule("HighScoreModule", (builder) => {
+  // Get the proxy from the previous module
+  const { proxy } = builder.useModule(ProxyModule);
+
+  // Create a contract instance using the deployed proxy's address
+  const instance = builder.contractAt("HighScore", proxy);
+
+  return { instance, proxy };
 });
 
 export default HighScoreModule;
